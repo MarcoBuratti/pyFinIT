4947e7a043156ab794b2a2c59f58c265271a988b
lucac000 <lucacot1707@gmail.com>
2020-11-03 11:19:21 +0100

    upupdate

--- Luca/stock.py
+++ Luca/stock.py
@@ -1,31 +1,39 @@
 from pandas_datareader import data as wb
 import pandas as pd
+from matplotlib import pyplot as pl
+import numpy as np
 
 tickers = ['TSLA', 'AAPL', 'VGT', 'ORCL']
 mydata = pd.DataFrame()
 for t in tickers:
     mydata[t]= wb.DataReader(t,data_source='yahoo', start='2010-1-1')['Adj Close']
 print(mydata)
+
 #ordinary check
 print(mydata.info())
 print(mydata.head())
 print(mydata.tail())
+
 #normalization to 100
 print(mydata.iloc[0])
 trend = (mydata/mydata.iloc[0]*100)
-from matplotlib import pyplot as pl
+
 #plot standardized trend
 trend.plot(figsize = (16,8))
+
 #plot normal trend
 mydata.plot(figsize=(16,8))
+
 #calculate a portfolio RR
 returns = (mydata/ mydata.shift(1)) -1
 print(returns.tail())
+
 #weights portfolio's stocks
 weights = np.array([0.25,0.25,0.25,0.25])
 annual_returns= returns.mean() *250
 print(annual_returns)
-import numpy as np
+
 print(np.dot(annual_returns,weights))
 portfolio = str(round(np.dot(annual_returns, weights),5)*100)+'%'
+
 print(portfolio)
