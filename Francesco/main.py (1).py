114303f86ecd43433156fc46175fe0adc7ecf656
FrancescoPinardi <francesco.pinardi@mip.polimi.it>
2020-11-12 11:34:45 +0100

    update CAPM
    Attacco us 10 yield al CAPM

--- Francesco/main.py (1).py
+++ Francesco/main.py (1).py
@@ -7,7 +7,11 @@ import pandas as pd
 from pandas_datareader import data as wb
 
 
-#TEST NUMBER 1: CAPM
+
+
+# CAPM W/ 4 STOCKS PORTFOLIO
+
+
 
 
 #Import last 5 years data (add market portfolio S&P 500)
@@ -18,9 +22,13 @@ for t in tickers:
 print(mydata2)
 
 
+
+
 #Calculate the log return of 4 stocks
 sec_returns = np.log(mydata2 / mydata2.shift(1))
 print(sec_returns)
+
+
 
 
 #Calculate the covariance matrix between portfolio and the Market
@@ -28,14 +36,20 @@ cov = sec_returns.cov()*250
 print(cov)
 
 
+
+
 #Calculate the covariance between the stock and the market as a float
 cov_with_market = cov.iloc[0,1]
 print(cov_with_market) 
 
 
+
+
 #Compute the annualized variance of the market S&P500
 market_var = sec_returns["^GSPC"].var()*250
 print(market_var)
+
+
 
 
 #Calculate the beta of the portfolio
@@ -46,12 +60,30 @@ print(beta)
 #This value is x > 1. This means that is an offensive portfolio
 
 
+
+
+#Install and import package
+pip install yahoofinancials
+from yahoofinancials import YahooFinancials
+
+
+
+
+#Import the current yield for treasury US govt bonds (no %)
+yahoo_financials = YahooFinancials('^TNX')
+yr_10 = yahoo_financials.get_current_price()/100
+
+
+
+
 #Calculating the expected return of the portfolio (CAPM)
-er = 0.025 + beta*0.05
+er = yr_10 + beta*0.05
 print("Expected Return:")
 print(er)
 
 
+
+
 #Calculate the sharpe ratio of the portfolio
-Sharpe = (er - 0.025) / (sec_returns[["TSLA", "AAPL", "VGT", "ORCL"]].std()*250**0.5)
+Sharpe = (er - yr_10) / (sec_returns[["TSLA", "AAPL", "VGT", "ORCL"]].std()*250**0.5)
 print(Sharpe)
