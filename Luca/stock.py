1176609da6c59a1ba02ab4d6b00646e6548abeda
lucac000 <lucacot1707@gmail.com>
2020-11-15 17:12:22 +0100

    paripariparipiri

--- Luca/stock.py
+++ Luca/stock.py
@@ -62,38 +62,39 @@ pfCorrMatrix = pfCorr(mydata)
 #Markowitz Portfolio Optimization
 
 
-#log_returns = np.log(mydata / mydata.shift(1))
+log_returns = np.log(mydata / mydata.shift(1))
 
-#log_returns.cov() *250
+log_returns.cov() *250
 
-#log_returns.corr()
+log_returns.corr()
 
-#num_assets = len(tickers)
+num_assets = len(tickers)
 
 #generate a for loop which gives back 1000 pf weights
 #sum of these random number must be equal to 1(sum of pf assets)
 #w /= sum.(w) is equivalent to w = w / sum.(w)->w1/(w1+w2) + w2/(w1+w2..) + wn/(sum.wn)=1
 
 
-#pf_returns= []
-#pf_vol= []
-#weig_list = []
+pf_returns= []
+pf_vol= []
+weig_list = []
 
 
 
-#for x in range(1000):
- #   weights_m = np.random.random(num_assets)
-  #  weights_m /= np.sum(weights_m)
-   # weig_list.append(weights_m)
-    #pf_returns.append(np.sum(weights_m*log_returns.mean()) *250)
-    #pf_vol.append(np.sqrt(np.dot(weights_m.T,np.dot(log_returns.cov() *250, weights_m))))
+for x in range(1000):
+    weights_m = np.random.random(num_assets)
+    weights_m /= np.sum(weights_m)
+    weig_list.append(weights_m)
+    pf_returns.append(np.sum(weights_m*log_returns.mean()) *250)
+    pf_vol.append(np.sqrt(np.dot(weights_m.T,np.dot(log_returns.cov() *250, weights_m))))
 
-#pf_returns = np.array(pf_returns)
 
-#pf_vol = np.array(pf_vol)
+weig_list = [np.round(num,4) for num in weig_list]
+pf_returns = np.array(pf_returns)
+pf_vol = np.array(pf_vol)
 
 #create a dataframe containings three features
-#portfolios = pd.DataFrame({'Return': pf_returns, 'Volatility': pf_vol, 'weig_list': weig_list})
+portfolios = pd.DataFrame({'Return': pf_returns, 'Volatility': pf_vol, 'weig_list': weig_list})
 
 
 #plot markoviz frontieer
@@ -104,31 +105,26 @@ pfCorrMatrix = pfCorr(mydata)
 
 #generate a function which gives back the three best pf for three different risk avversion class(Low,Avg,High risk avv.)
 
-#def bestMarkPf(portfolios):
- #   maxRet = portfolios['Return'].argmax()
-  #  minVol = portfolios['Volatility'].argmin()
-   # avgRet = (portfolios['Return'].argmax()//3)
-    #return maxRet, minVol, avgRet
-
+def bestMarkPf(portfolios):
+    maxRet = portfolios['Return'].argmax()
+    minVol = portfolios['Volatility'].argmin()
+    avgRet = (portfolios['Return'].argmax()//3)
+    return maxRet, minVol, avgRet
 
 
 #attach three values to the three pf
 
-#maxReturnPf, minVolatilityPf, avgReturnPf = bestMarkPf(portfolios)  
+maxReturnPf, minVolatilityPf, avgReturnPf = bestMarkPf(portfolios)  
 
 
-#print low risk adversion pf
-
-#print('Max return-risk pf', portfolios.iloc[maxReturnPf])
-
-
-#print avg risk adversion pf
-
-#print('Avg return-risk pf',portfolios.iloc[avgReturnPf])
+pfpuntoMaxRet = portfolios.iloc[maxReturnPf]
+pfpuntoMinVol  = portfolios.iloc[minVolatilityPf]
+PfpuntoAvgRet = portfolios.iloc[avgReturnPf]
 
 
-#print high risk adversion pf
-
-#print('min return-risk pf',portfolios.iloc[maxReturnPf])
-
-
+#plot efficient frontier 
+portfolios.plot(x='Volatility', y='Return', kind='scatter', figsize=(10,6))
+plt.scatter(x=pfpuntoMaxRet['Volatility'],y=pfpuntoMaxRet['Return'], c='r')
+plt.scatter(x=pfpuntoMinVol['Volatility'],y=pfpuntoMinVol['Return'], c='r')
+plt.scatter(x=pfpuntoAvgRet['Volatility'],y=pfpuntoAvgRet['Return'], c='r')
+plt.savefig('../img/frontier.png')
