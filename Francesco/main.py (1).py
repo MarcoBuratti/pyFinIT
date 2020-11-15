cc90862bb741d95d7bcd9a0692a0b32f77285547
Marco Buratti <ocr@Marcos-MacBook-Pro.local>
2020-11-15 18:45:13 +0100

    update

--- Francesco/main.py (1).py
+++ Francesco/main.py (1).py
@@ -1,14 +1,9 @@
-#!/usr/bin/env python
-# coding: utf-8
-
 #Import packages
 import numpy as np
 import pandas as pd
 from pandas_datareader import data as wb
 
-
-
-
+#from Stock_Functions import * 
 # CAPM W/ 4 STOCKS PORTFOLIO
 
 
@@ -19,30 +14,22 @@ tickers = ['TSLA', 'AAPL', 'VGT', 'ORCL', '^GSPC']
 mydata2 = pd.DataFrame()
 for t in tickers:
     mydata2[t]= wb.DataReader(t,data_source='yahoo', start='2015-11-1', end = "2020-10-31")['Adj Close']
-print(mydata2)
+
 
 
 
 
 #Calculate the log return of 4 stocks
 sec_returns = np.log(mydata2 / mydata2.shift(1))
-print(sec_returns)
+#print(sec_returns)
 
 
 
 
 #Calculate the covariance matrix between portfolio and the Market
 cov = sec_returns.cov()*250
-print(cov)
-
-
-
-
-#Calculate the covariance between the stock and the market as a float
-cov_with_market = cov.iloc[0,1]
-print(cov_with_market) 
-
-
+#Take the first row ov covariance
+cov_with_market = cov.iloc[0:1]
 
 
 #Compute the annualized variance of the market S&P500
@@ -63,7 +50,6 @@ print(beta)
 
 
 #Install and import package
-pip install yahoofinancials
 from yahoofinancials import YahooFinancials
 
 
@@ -71,19 +57,17 @@ from yahoofinancials import YahooFinancials
 
 #Import the current yield for treasury US govt bonds (no %)
 yahoo_financials = YahooFinancials('^TNX')
-yr_10 = yahoo_financials.get_current_price()/100
+yr_10 = yahoo_financials.get_current_price() / 100
 
 
 
 
 #Calculating the expected return of the portfolio (CAPM)
-er = yr_10 + beta*0.05
+er = yr_10 + beta * 0.05
 print("Expected Return:")
 print(er)
 
 
-
-
 #Calculate the sharpe ratio of the portfolio
-Sharpe = (er - yr_10) / (sec_returns[["TSLA", "AAPL", "VGT", "ORCL"]].std()*250**0.5)
+Sharpe = (er - yr_10) / (sec_returns[["TSLA", "AAPL", "VGT", "ORCL"]].std() * 250**0.5 )
 print(Sharpe)
